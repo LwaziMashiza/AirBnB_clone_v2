d0334782125e50d5fe05377eac1c4094654499b5
root <lwaziluthuli0@gmail.com>
2023-12-07 13:50:33 -0800

    File completed

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
-# Fabfile to delete out-of-date archives.
-import os
+"""
+Fabric script (based on the file 2-do_deploy_web_static.py) that creates and
+distributes an archive to your web servers, using the function deploy
+"""
+from datetime import datetime
 from fabric.api import *
+import shlex
+import os
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+
+env.hosts = ['34.148.170.206', '3.226.243.40']
+env.user = "ubuntu"
 
 
 def do_clean(number=0):
-    """Delete out-of-date archives.
+    """ Clean archives """
 
-    Args:
-        number (int): The number of archives to keep.
+    number = int(number)
 
-    If number is 0 or 1, keep only the most recent archive. If
-    number is 2, keep the most and second-most recent archives.
-    """
-    number = 1 if int(number) == 0 else int(number)
+    if number == 0:
+        number = 2
+    else:
+        number += 1
 
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
-
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
-
+    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    path = '/data/web_static/releases'
+    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
